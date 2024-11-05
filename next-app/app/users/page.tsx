591f24cc5224d8cc5fb4edaf9bf457299dbe958d
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2024-11-05 02:53:24 +0000

    Added logic to fetch users from jsonplaceholder.typicode.com using a server-side component and the fetch api thats comes with nextjs

--- next-app/app/users/page.tsx
+++ next-app/app/users/page.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
-const UsersPage = () => {
+interface User {
+ id: number;
+ name: string;
+}
+
+const UsersPage = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const users: User[] = await res.json();
   return (
     <div>
-      UsersPage
+      <h1>UsersPage</h1>
+      <ul>
+        {users.map(({id, name}) => <li key={id}>{name}</li>)}
+      </ul>
     </div>
   )
 }
