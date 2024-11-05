2291400884113805ff772551fc834560f1925c37
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2024-11-05 02:34:59 +0000

    Isolated an AddToCart button to make it a client side component, this makes the application faster

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+import ProductCard from "./components/ProductCard";
 
 
 export default function Home() {
   return (
-   <h1>Hello World</h1>
+   <main>
+    <h1>Hello World</h1>
+    <Link href='/users' className="bg-green-500 px-3 py-2 rounded-md m-10">Users</Link>
+    <ProductCard />
+   </main>
   )
 }
