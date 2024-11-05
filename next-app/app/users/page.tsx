de6541bd30cbed4d33d935dd6c76388265de923c
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2024-11-05 03:27:15 +0000

    Demonstrated the difference between static and dynamic components, which is all about the fetch api provided by next js, which allows cacheing options of whether to 'force-cache' or 'no-store' - which informs if the page is dynamic or static, respecitvely.

--- next-app/app/users/page.tsx
+++ next-app/app/users/page.tsx
@@ -6,11 +6,14 @@ interface User {
 }
 
 const UsersPage = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const res = await fetch('https://jsonplaceholder.typicode.com/users', { cache: 'no-store' }
+        
+    );
     const users: User[] = await res.json();
   return (
     <div>
       <h1>UsersPage</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map(({id, name}) => <li key={id}>{name}</li>)}
       </ul>
